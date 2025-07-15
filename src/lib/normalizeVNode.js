db96d92a3701caaec56d517b6b142e17796a908e
sangsoo <wsckm1@gmail.com>
2025-07-15 16:05:43 +0900

    feat: normalizeVNode에서 Falsy 값 자식 제거 및 숫자/문자 변환 처리

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,28 @@
 export function normalizeVNode(vNode) {
+  // null, undefined, boolean 값은 빈 문자열로 변환되어야 한다.
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+    return "";
+  }
+
+  // 문자열과 숫자는 문자열로 변환되어야 한다.
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return String(vNode);
+  }
+
+  // vNode가 함수인 경우, 함수를 호출하여 새로운 vNode를 생성한다.
+  if (typeof vNode.type === "function") {
+    return normalizeVNode(vNode.type({ ...vNode.props, children: vNode.children }));
+  }
+
+  // vNode의 자식 vNode들을 재귀적으로 정규화한다.
+  if (Array.isArray(vNode.children)) {
+    const noramlizedChildren = vNode.children
+      .map(normalizeVNode)
+      .filter((child) => child !== "" && child !== null && child !== undefined && child !== false);
+    return {
+      ...vNode,
+      children: noramlizedChildren,
+    };
+  }
   return vNode;
 }
