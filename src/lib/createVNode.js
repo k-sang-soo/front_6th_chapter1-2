dd51eb4b5d415288da877467dc7ff9ff061b5acb
sangsoo <wsckm1@gmail.com>
2025-07-15 14:47:08 +0900

    chore: createVNode 주석 추가

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,4 +1,6 @@
 export function createVNode(type, props, ...children) {
+  // children은 배열이므로 flat을 통해 하나의 배열로 만들어준다. (평탄화 작업)
+  // filter를 통해 null, undefined, false를 제거한다.
   return {
     type,
     props,
