f8a6cd2f172e61baafe7b2909e9fbcc31a0769d1
sangsoo <wsckm1@gmail.com>
2025-07-16 17:44:35 +0900

    feat(renderElement.js): 로직 개선
    - DOM 업데이트 여부를 판단하기 위해 container에 vNode 캐싱 추가

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,10 +1,25 @@
-import { setupEventListeners } from "./eventManager";
-import { createElement } from "./createElement";
-import { normalizeVNode } from "./normalizeVNode";
-import { updateElement } from "./updateElement";
+import { normalizeVNode } from "./normalizeVNode.js";
+import { createElement } from "./createElement.js";
+import { setupEventListeners } from "./eventManager.js";
+import { updateElement } from "./updateElement.js";
 
 export function renderElement(vNode, container) {
   // 최초 렌더링시에는 createElement로 DOM을 생성하고
   // 이후에는 updateElement로 기존 DOM을 업데이트한다.
   // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  const normalizedVNode = normalizeVNode(vNode);
+
+  const oldNode = container._vNode;
+  const isFirstRender = !oldNode;
+
+  if (isFirstRender) {
+    container.appendChild(createElement(normalizedVNode));
+  } else {
+    updateElement(container, normalizedVNode, oldNode);
+  }
+
+  container._vNode = normalizedVNode;
+  setupEventListeners(container);
+
+  return container;
 }
